1261ec873d0a23eb629be58c153588c37d3eb00d
OldBrain <hell_brain@mail.ru>
2021-02-09 21:04:42 +0400

    Initial commit

---
+++ desktop/src/ru/geeckbrains/stargame/desktop/DesktopLauncher.java
@@ -0,0 +1,16 @@
+package ru.geeckbrains.stargame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geeckbrains.stargame.StartGame;
+
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 600;
+		config.width = 400;
+		config.resizable = false;
+		new LwjglApplication(new StartGame(), config);
+	}
+}
