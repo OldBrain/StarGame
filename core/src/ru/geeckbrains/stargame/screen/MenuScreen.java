87c0a5548c1690f084eff3320c74ffbe5092ad0a
OldBrain <hell_brain@mail.ru>
2021-02-10 02:42:06 +0400

    Поворот, движение и остановка спрайта.

--- core/src/ru/geeckbrains/stargame/screen/MenuScreen.java
+++ core/src/ru/geeckbrains/stargame/screen/MenuScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.math.Vector2;
 import ru.geeckbrains.stargame.base.BaseScreen;
 import ru.geeckbrains.stargame.math.Rect;
 import ru.geeckbrains.stargame.sprite.Background;
+import ru.geeckbrains.stargame.sprite.StarShip;
 
 
 public class MenuScreen extends BaseScreen {
@@ -14,35 +15,48 @@ public class MenuScreen extends BaseScreen {
     private Texture bg;
     private Background background;
 
+    private Texture starShipTexture;
+    private StarShip starShip;
+
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+
+        starShipTexture = new Texture("textures/sh.png");
+        starShip = new StarShip(starShipTexture);
     }
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0.3f, 0.6f, 0.4f, 1);
+        Gdx.gl.glClearColor(0.3f, 0.0f, 0.0f, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
         background.draw(batch);
+        starShip.draw(batch);
         batch.end();
+        starShip.update(delta);
     }
 
     @Override
     public void dispose() {
         bg.dispose();
+        starShipTexture.dispose();
         super.dispose();
     }
 
     @Override
     public void resize(Rect worldBounds) {
         background.resize(worldBounds);
+
+        starShip.resize(worldBounds);
+
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
+        starShip.touchDown(touch, pointer, button);
         return super.touchDown(touch, pointer, button);
     }
 }
